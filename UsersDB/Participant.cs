205104a47d8980ce3fdc8e09a558d009c8ec6489
Vladislav Pavlyuk <vladpavliuk@gmail.com>
2024-01-16 13:23:32 +0100

    #11 Изменена схема БД. Добавлены Rooms и Participants

---
+++ UsersDB/Participant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersDB
+{
+    public partial class Participant
+    {
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime LastRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime DeleteedAt { get; set; }
+
+        public virtual Room Room { get; set; } = null!;
+
+        public virtual Users User { get; set; } = null!;
+    }
+}
---
+++ UsersDB/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersDB
+{
+    public partial class Room
+    {
+        public int Id { get; set; }
+
+        public string? Subject { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime DeletedAt { get; set; }
+
+        public virtual ICollection<Messages> Messages { get; set; } = new List<Messages>();
+
+        public virtual ICollection<Participant> Participants { get; set; } = new List<Participant>();
+    }
+}
